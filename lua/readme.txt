6693ab114d06e8515c84a69983e5fd71d02ce3b7
Yury Benesh <ybxsoft@tut.by>
2013-05-25 13:28:38 +0300

    Some lua-luajit cleanup

---
+++ lua/readme.txt
@@ -0,0 +1,1 @@
+This directory contains non-working lua implementation, probably with luajit specific code. It's no longer used.
--- luajit/bufimg.lua
+++ luajit/bufimg.lua
@@ -58,40 +58,6 @@ function buf_img_save(img, fn)
     f:close()
 end
 
-local rem = [[
-    buf_img * buf_img_load(char * fn);
-    void buf_img_save(buf_img * img, char * fn);
-    void buf_img_release(buf_img * b);
 
-    //int buf_img_ofs(buf_img * img, int x, int y);
-    //bool buf_img_is_in_bounds(buf_img * img, int x, int y);
-    
-    inline int buf_img_ofs(buf_img * img, int x, int y) {
-        return img->w*y*4 + x*4;
-    }
-
-
-    inline bool buf_img_is_in_bounds(buf_img * img, int x, int y) {
-        return ((x > 0) && (x < img->w) && (y > 0) && (y < img->h));
-    }
-    
-    //int buf_img_get_width(buf_img * img);
-    //int buf_img_get_height(buf_img * img);
-    
-    //void buf_img_set_pixel(buf_img * img, int ofs, const trgba * v);
-    //void buf_img_get_pixel(buf_img * img, int ofs, trgba * out_v);
-    
-    inline void buf_img_set_pixel(buf_img * img, int ofs, const trgba * v) {
-        for(int i = 0; i < 4; ++i)
-            img->buf[ofs + i] = v->rgba[i];
-    }
-
-
-    inline void buf_img_get_pixel(buf_img * img, int ofs, trgba * out_v) {
-        for(int i = 0; i < 4; ++i)
-            out_v->rgba[i] = img->buf[ofs + i];
-    }
-    
-]]
 
 
