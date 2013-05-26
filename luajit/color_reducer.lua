21fac4cb2ab585f5417b784455129b67de75b86d
Yury Benesh <ybxsoft@tut.by>
2013-05-26 18:36:10 +0300

    color_reducer in lua

---
+++ luajit/color_reducer.lua
@@ -0,0 +1,47 @@
+ffi = require 'ffi'
+bit = require 'bit'
+
+-----
+
+local M = {}
+
+local ColorReducer = {}
+M.ColorReducer = ColorReducer
+
+local downgrade4lookup = ffi.new("int32_t[?]", 256)
+
+local function downgrade4444(a, cNum)
+    return downgrade4lookup[a]
+end
+
+
+function ColorReducer.create()
+    initLookups()
+    local inst = {}
+    setmetatable(inst, {__index = ColorReducer})
+    inst.downgr = downgrade4444
+    return inst    
+end
+
+
+function ColorReducer:reduceToClosest(rgba, destRGBA)
+    for i = 0,3 do
+        destRGBA[i] = self.downgr(rgba[i], i)
+    end
+end
+
+
+local function downgrade(a, targetBitCount)
+    local maxv = bit.lshift(1, targetBitCount) - 1;
+    return a * maxv / 255 * 255 / maxv
+end
+
+
+local function initLookups()
+    for i = 0, 255 do
+        downgrade4lookup[i] = downgrade(i, 4)
+    end
+end
+
+------
+return M
